2f8c831954bd8e18dc7f434646324f9e83104f22
Camber Huang <camber@poi.science>
2025-01-29 23:48:48 +0800

    [WIP, DO NOT MERGE] documenting

--- pyLuogu/api.py
+++ pyLuogu/api.py
@@ -9,7 +9,16 @@ from .types import *
 from .errors import *
 from . import logger
 
+__COMMON_UA = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133."
+
 class luoguAPI:
+    """ Luogu API interface interacting via Requests
+
+    Attributes:
+        base_url (str): The base URL for the Luogu API. Defaults to "https://www.luogu.com.cn".
+        cookies (LuoguCookies or None)
+
+    """
     def __init__(
             self,
             base_url="https://www.luogu.com.cn",
@@ -143,7 +152,8 @@ class luoguAPI:
                 logger.debug(f"Response: {response.text}")
 
                 response.raise_for_status()
-
+                
+                # Extracting csrf from meta
                 soup = bs4.BeautifulSoup(response.text, "html.parser")
                 csrf_meta = soup.select_one("meta[name='csrf-token']")
 
