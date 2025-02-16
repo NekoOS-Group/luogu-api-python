fba9372870579952848d84640f4f3170a7b9bb63
Camber Huang <camber@poi.science>
2025-02-16 09:37:34 +0800

    docs: initial commit for sphinx

---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'luogu-api-python'
+copyright = '2025, bzy-nya<bzy-mic@qq.com>, Camber Huang<camber@poi.science>'
+author = 'bzy-nya<bzy-mic@qq.com>, Camber Huang<camber@poi.science>'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
